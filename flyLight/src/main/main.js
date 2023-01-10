cd73072f413dbb5feb806b7c77dd048da0c3ae70
xudan <875126243@qq.com>
2023-01-10 15:49:04 +0800

    fix update

--- flyLight/src/main/main.js
+++ flyLight/src/main/main.js
@@ -1,46 +1,85 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import gsap from 'gsap'
-// import  *  as dat from "dat.gui"
+import gsap from 'gsap'
+import  *  as dat from "dat.gui"
+
+const gui = new dat.GUI();
 // 顶点着色器
-import deepVertexShader from "../shaders/flylight/vertex.glsl";
+import vertexShader from "../shaders/flylight/vertex.glsl";
 // 片元着色器
-import deepFragmentShader from "../shaders/flylight/fragment.glsl";
+import fragmentShader from "../shaders/flylight/fragment.glsl";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // 创建场景
 const scene = new THREE.Scene();
 // 创建相机
 const width = window.innerWidth;
 const height = window.innerHeight;
-const camera = new THREE.PerspectiveCamera(15, width/height, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(
+    90,
+    window.innerHeight / window.innerHeight,
+    0.1,
+    1000
+);
+
+// 创建纹理加载器对象
+const rgbeLoader = new RGBELoader();
+rgbeLoader.loadAsync("./assets/2k.hdr").then((texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
+
 // 设置相机位置
-camera.position.set(0, 0 , 10)
+camera.position.set(0, 0, 2);
 scene.add(camera)
 
-const textureLoader = new THREE.TextureLoader()
-const texture = textureLoader.load("1.png")
+// const textureLoader = new THREE.TextureLoader()
+// const texture = textureLoader.load("1.png")
 
-const rawShaderMaterial = new THREE.RawShaderMaterial({
-    // 顶点着色器
-    vertexShader: deepVertexShader,
-    // 片元着色器
-    fragmentShader: deepFragmentShader,
+const shaderMaterial = new THREE.RawShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {},
     side: THREE.DoubleSide,
-    // wireframe: true,
-    uniforms: {
-        uTime: {
-            value: 0,
-        },
-        uTexture: {
-            value: texture
-        }
-    }
+    // transparent: true,
 })
 
-// 平面
-const geometry = new THREE.PlaneGeometry( 1, 1, 64, 64 );
-// const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-const plane = new THREE.Mesh( geometry, rawShaderMaterial );
-scene.add( plane );
+
+const gltfLoader = new GLTFLoader();
+let LightBox = null;
+gltfLoader.load("./assets/model/flyLight.glb", (gltf) => {
+//   scene.add(gltf.scene);
+  LightBox = gltf.scene.children[1];
+  LightBox.material = shaderMaterial;
+  for (let i = 0; i < 150; i++) {
+    let flyLight = gltf.scene.clone(true);
+    let x = (Math.random() - 0.5) * 300;
+    let z = (Math.random() - 0.5) * 300;
+    let y = Math.random() * 60 + 25;
+    flyLight.position.set(x, y, z);
+    gsap.to(flyLight.rotation, {
+        y: 2 * Math.PI,
+        duration: 10 + Math.random() * 30,
+        repeat: -1,
+    });
+    gsap.to(flyLight.position, {
+        x: "+=" + Math.random() * 5,
+        y: "+=" + Math.random() * 10,
+        yoyo: true,
+        duration: 5 + Math.random() * 10,
+        repeat: -1,
+    });
+    scene.add(flyLight);
+  }
+});
+
+
+// // 平面
+// const geometry = new THREE.PlaneGeometry( 1, 1, 64, 64 );
+// // const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+// const plane = new THREE.Mesh( geometry, rawShaderMaterial );
+// scene.add( plane );
 
 // 坐标轴的对象
 // const axesHelper = new THREE.AxesHelper( 6 );
@@ -49,17 +88,32 @@ scene.add( plane );
 // 渲染
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(width, height)
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.toneMapping = THREE.LinearToneMapping;
+// renderer.toneMapping = THREE.ReinhardToneMapping;
+// renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMappingExposure = 0.2;
 document.body.appendChild(renderer.domElement)
+
+
 
 // 控制器
 const controls =  new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
-
-const clock = new THREE.Clock();
+// 设置自动旋转
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.1;
+controls.maxPolarAngle = (Math.PI / 3) * 2;
+controls.minPolarAngle = (Math.PI / 3) * 2;
+// const clock = new THREE.Clock();
 
 function render() {
-    const elapsedTime = clock.getElapsedTime();
-    rawShaderMaterial.uniforms.uTime.value = elapsedTime;
+    // const elapsedTime = clock.getElapsedTime();
+    // rawShaderMaterial.uniforms.uTime.value = elapsedTime;
     controls.update()
     renderer.render(scene, camera)
     requestAnimationFrame(render)
--- flyLight/src/shaders/flylight/vertex.glsl
+++ flyLight/src/shaders/flylight/vertex.glsl
@@ -1,3 +1,19 @@
+precision lowp float;
+attribute vec3 position;
+attribute vec2 uv;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+varying vec4 vPosition;
+varying vec4 gPosition;
+
+varying vec2 vUv;
+
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1);
+    vec4 modelPosition = modelMatrix * vec4( position, 1.0 );
+
+    vPosition = modelPosition;
+    gPosition = vec4( position, 1.0 );
+    gl_Position =  projectionMatrix * viewMatrix * modelPosition;
 }
