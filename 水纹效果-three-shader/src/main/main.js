239406d8b0d247651939986f0e73d40529d0e486
xudan <875126243@qq.com>
2023-01-12 14:17:18 +0800

    fix update

--- 水纹效果-three-shader/src/main/main.js
+++ 水纹效果-three-shader/src/main/main.js
@@ -88,7 +88,6 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   //   更新摄像机的投影矩阵
   camera.updateProjectionMatrix();
-
   //   更新渲染器
   renderer.setSize(window.innerWidth, window.innerHeight);
   //   设置渲染器的像素比例
