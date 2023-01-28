c4bb784981249432fe60b2adde16254f0a4c2d47
xudan <875126243@qq.com>
2023-01-28 15:38:33 +0800

    fix update

---
+++ 360fang/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './view/common/Header.vue'
+import Footer from './view/common/Footer.vue'
+import Home from './view/home/Home.vue'
+</script>
+
+<template>
+    <Header />
+    <Home />
+    <Footer />
+</template>
---
+++ 360fang/src/view/home/Home.vue
@@ -0,0 +1,24 @@
+<script >
+import { defineComponent } from "vue";
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'Hello',
+  setup() {
+    const store = useStore();
+    const addAcountVuex = () => {
+      console.log("store", store.state)
+      store.commit('ADD_ACOUNTVUEX', store.state.appStore.countVuex + 1);
+    }
+    return {
+      addAcountVuex,
+      $store: store
+    };
+  },
+});
+</script>
+<template>
+  <div class="hello-wrap">
+    <!-- <button type="button" @click="addAcountVuex">count is: {{ $store.state.appStore.countVuex }}</button> -->
+  </div>
+</template>
